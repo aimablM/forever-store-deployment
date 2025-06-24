c5346d2a23118300f22e2ee8650b867aebb6fe52
Aimable Mugwaneza <aimable.mugwaneza@gmail.com>
2025-06-24 01:47:46 -0400

    -Ashley footer message added

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -9,7 +9,7 @@ const Footer = () => {
         <div>
             <img src={assets.logo} className='mb-5 w-32' alt="" />
             <p className='w-full md:w-2/3 text-gray-600'>
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+            Inspired by Ashley 😉✨ — whose warmth, creativity, and intentional spirit sparked the heart behind this project. From thoughtful details to future dreams, this store is a quiet thank-you for the way you move through the world. You make ideas feel possible. 
             </p>
         </div>
 
